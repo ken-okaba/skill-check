614184d88076b3bbd5eed750c857ac25eb6419e6
t-ken <t-ken@DESKTOP-FIG9F49>
2020-05-01 16:40:23 +0900

    Q007：完成（仮）

--- src/main/java/q007/Q007.java
+++ src/main/java/q007/Q007.java
@@ -1,5 +1,14 @@
 package q007;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * q007 最短経路探索
  *
@@ -26,5 +35,122 @@ XXXXXXXXX
 14
  */
 public class Q007 {
+    public static void main(String[] args) {
+
+        MazeInputStream mis = new MazeInputStream();
+
+        System.err.println(
+                new BufferedReader(new InputStreamReader(mis)).lines()
+                        .collect(Collectors.joining(System.lineSeparator())));
+
+        mis.reset();
+
+        String[][] ss = new BufferedReader(new InputStreamReader(mis)).lines()
+                .map(line -> Stream.of(line.split("")).toArray(String[]::new)).toArray(String[][]::new);
+
+        Coordinate start = xxxxxx(ss);
+
+        List<List<Coordinate>> list = new ArrayList<>();
+        List<Coordinate> listX = new ArrayList<>();
+        listX.add(start);
+        while (true) {
+            listX = aaa(ss, list, listX);
+            list.add(listX);
+            System.err.println("list: " + list);
+        }
+    }
+
+    private static Coordinate xxxxxx(String[][] ss) {
+        Coordinate start = null;
+        for (int y = 0; y < ss.length; y++) {
+            String[] strings = ss[y];
+            for (int x = 0; x < strings.length; x++) {
+                if (strings[x].equals("S")) {
+                    start = new Coordinate(x, y);
+                }
+            }
+        }
+        return start;
+    }
+
+    private static List<Coordinate> aaa(String[][] ss, List<List<Coordinate>> list, List<Coordinate> list0) {
+        List<Coordinate> list1 = new ArrayList<>();
+        Optional<Coordinate> xy = extracted(ss, list, list0, list1);
+        xy.ifPresent(val -> {
+            System.out.println(list.size() + 1);
+            System.exit(0);
+        });
+        if (list1.isEmpty()) {
+            System.out.println("-1");
+            System.exit(-1);
+        }
+        return list1;
+    }
+
+    private static Optional<Coordinate> extracted(String[][] ss, List<List<Coordinate>> list, List<Coordinate> list0,
+            List<Coordinate> list1) {
+        Optional<Coordinate> xy = list0.stream()
+                .map(coo -> aaa(ss, coo, list1, list))
+                .filter(Optional::isPresent)
+                .findFirst().flatMap(UnaryOperator.identity());
+        return xy;
+    }
+
+    private static Optional<Coordinate> aaa(String[][] ss, Coordinate coo, List<Coordinate> list,
+            List<List<Coordinate>> list0) {
+        Stream<Coordinate> st = Stream.of(new Coordinate(coo.getX(), coo.getY() - 1),
+                new Coordinate(coo.getX() + 1, coo.getY()),
+                new Coordinate(coo.getX(), coo.getY() + 1), new Coordinate(coo.getX() - 1, coo.getY()));
+
+        return st.filter(xy -> list0.stream().noneMatch(l -> l.contains(xy))).filter(xy -> extracted(ss, xy, list))
+                .findFirst();
+    }
+
+    private static boolean extracted(String[][] ss, Coordinate coo, List<Coordinate> list) {
+        String s = ss[coo.getY()][coo.getX()];
+        if (s.equals("E")) {
+            return true;
+        }
+
+        if (s.equals(" ")) {
+            list.add(coo);
+        }
+        return false;
+    }
+
+    public static class Coordinate {
+        private final int x;
+        private final int y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj instanceof Coordinate) {
+                Coordinate other = (Coordinate) obj;
+                return other.x == x && other.y == y;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + x + "," + y + "}";
+        }
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 02時間 14分
