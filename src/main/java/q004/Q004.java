e6b9c4f4175ee5be9de7dd4636ba055e8a45017a
t-ken <t-ken@DESKTOP-FIG9F49>
2020-05-01 12:18:00 +0900

    Q004：完成

--- src/main/java/q004/Q004.java
+++ src/main/java/q004/Q004.java
@@ -15,5 +15,16 @@ package q004;
  * - ListManagerクラスの比較 compare と入れ替え exchange を使って実現してください
  */
 public class Q004 {
+    public static void main(String[] args) {
+        ListManager data = new ListManager();
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = data.size() - 1; j > i; j--) {
+                if (data.compare(j - 1, j) == 1) {
+                    data.exchange(j - 1, j);
+                }
+            }
+        }
+        data.checkResult();
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 00時間 05分
