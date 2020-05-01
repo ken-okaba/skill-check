53ffaaa9fe371757ad04e7a161f7d3cf5cfd91ad
t-ken <t-ken@DESKTOP-FIG9F49>
2020-05-01 17:07:31 +0900

    Q008：完成

--- src/main/java/q008/Q008.java
+++ src/main/java/q008/Q008.java
@@ -1,10 +1,11 @@
 package q008;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
@@ -27,14 +28,36 @@ Q004.java(13): Pattern pattern = Pattern.compile("(\".*\")|(\'.*\')");
 
  */
 public class Q008 {
+
+    private static final Pattern p = Pattern.compile("\".*\"|'.+'");
+
     /**
      * JavaファイルのStreamを作成する
      *
      * @return
-     * @throws IOException
+     * @throws UncheckedIOException
      */
-    private static Stream<File> listJavaFiles() throws IOException {
-        return Files.walk(Paths.get(".")).map(Path::toFile).filter(f -> f.getName().endsWith(".java"));
+    private static Stream<Path> listJavaFiles() {
+        try {
+            return Files.walk(Paths.get(".")).filter(f -> f.toString().endsWith(".java"));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        listJavaFiles().forEach(Q008::print);
+    }
+
+    private static void print(Path path) {
+        try {
+            int[] num = { 0 };
+            Files.lines(path).peek(line -> num[0]++).filter(line -> p.matcher(line).find())
+                    .map(line -> String.format("%s(%d): %s", path.getFileName(), num[0], line.trim()))
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 00時間 18分
